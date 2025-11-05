d51b85c34ef0a976540312e230bca80b42ff905a
SimY4 <alex.simkin@outlook.com>
2025-11-05 21:18:09 +1100

    y2025 q2.

---
+++ src/main/scala/everybody/codes/y2025/Quest2.scala
@@ -0,0 +1,40 @@
+package everybody.codes.y2025
+
+object Quest2:
+  final private case class Complex(x: Long, y: Long):
+    def +(other: Complex): Complex = Complex(x + other.x, y + other.y)
+    def *(other: Complex): Complex = Complex(x * other.x - y * other.y, x * other.y + y * other.x)
+    def /(other: Complex): Complex = Complex(x / other.x, y / other.y)
+    override def toString: String  = s"[$x,$y]"
+
+  def solve(input: String): String =
+    val s"A=[$x,$y]" = input
+    val A            = Complex(x.toLong, y.toLong)
+
+    Iterator
+      .iterate(Complex(0, 0)) { v =>
+        v * v / Complex(10, 10) + A
+      }
+      .drop(3)
+      .next
+      .toString
+
+  def solve2(input: String): Int =
+    val s"A=[$x,$y]" = input
+    val A            = Complex(x.toLong, y.toLong)
+
+    (for
+      y <- A.y to A.y + 1000 by 10
+      x <- A.x to A.x + 1000 by 10
+      check = Iterator
+        .iterate(Complex(0, 0)) { v =>
+          v * v / Complex(100000, 100000) + Complex(x, y)
+        }
+        .drop(100)
+        .next
+      if check.x.abs < 1000000 && check.y.abs < 1000000
+    yield check).size
+
+  val input = "A=[160,58]"
+
+  val input2 = "A=[-4571,-68892]"
